43b9c2e02737a8e8eaa4a726f768685b9f1ab1e5
XHB911 <2868917285@qq.com>
2022-08-04 23:37:16 +0800

    build project frame

---
+++ aboo/log.cpp
@@ -0,0 +1,33 @@
+#include "log.h"
+
+namespace aboo {
+
+Logger(const std::string& name = "root") {
+
+}
+
+void Logger::log(LogLevel::Level level, LogEvent::ptr event) {
+
+}
+
+void Logger::debug(LogEvent::ptr event) {
+
+}
+
+void Logger::info(LogEvent::ptr event) {
+
+}
+
+void Logger::warn(LogEvent::ptr event) {
+
+}
+
+void Logger::error(LogEvent::ptr event) {
+
+}
+
+void Logger::fatal(LogEvent::ptr event) {
+
+}
+
+}

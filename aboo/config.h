c8874c3925a89ff238842904ad975a76190d44c5
XHB911 <2868917285@qq.com>
2022-08-23 10:46:42 +0800

    日志系统——config添加yaml配置文件整合STL库类型、自定义类型

--- aboo/config.h
+++ aboo/config.h
@@ -7,12 +7,20 @@
 #include <boost/lexical_cast.hpp>
 #include <yaml-cpp/yaml.h>
 #include "log.h"
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
 
 namespace aboo {
 
 class ConfigVarBase {
 public:
 	typedef std::shared_ptr<ConfigVarBase> ptr;
+
 	ConfigVarBase(const std::string& name, const std::string& description = "") : m_name(name), m_description(description) {
 		std::transform(m_name.begin(), m_name.end(), m_name.begin(), ::tolower);
 	}
@@ -21,20 +29,214 @@ public:
 	const std::string& getDescription() const { return m_description; }
 	virtual std::string toString() = 0;
 	virtual bool fromString(const std::string& val) = 0;
+	virtual std::string getTypeName() const = 0;
 protected:
 	std::string m_name;
 	std::string m_description;
 };
 
+// F from_type, T to_type
+template<class F, class T>
+class LexicalCast {
+public:
+	T operator() (const F& v) {
+		return boost::lexical_cast<T>(v);
+	}
+};
+
 template<class T>
+class LexicalCast<std::string, std::vector<T> > {
+public:
+	std::vector<T> operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		typename std::vector<T> vec;
+		std::stringstream ss;
+		for (size_t i = 0; i < node.size(); ++i) {
+			ss.str("");
+			ss << node[i];
+			vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+		}
+		return vec;
+	}
+};
+
+template<class T>
+class LexicalCast<std::vector<T>, std::string> {
+public:
+	std::string operator() (const std::vector<T>&  v) {
+		YAML::Node node;
+		for (auto& i : v) {
+			node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+		}
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+template<class T>
+class LexicalCast<std::string, std::list<T> > {
+public:
+	std::list<T> operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		typename std::list<T> vec;
+		std::stringstream ss;
+		for (size_t i = 0; i < node.size(); ++i) {
+			ss.str("");
+			ss << node[i];
+			vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+		}
+		return vec;
+	}
+};
+
+template<class T>
+class LexicalCast<std::list<T>, std::string> {
+public:
+	std::string operator() (const std::list<T>&  v) {
+		YAML::Node node;
+		for (auto& i : v) {
+			node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+		}
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+template<class T>
+class LexicalCast<std::string, std::set<T> > {
+public:
+	std::set<T> operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		typename std::set<T> vec;
+		std::stringstream ss;
+		for (size_t i = 0; i < node.size(); ++i) {
+			ss.str("");
+			ss << node[i];
+			vec.insert(LexicalCast<std::string, T>()(ss.str()));
+		}
+		return vec;
+	}
+};
+
+template<class T>
+class LexicalCast<std::set<T>, std::string> {
+public:
+	std::string operator() (const std::set<T>&  v) {
+		YAML::Node node;
+		for (auto& i : v) {
+			node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+		}
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+template<class T>
+class LexicalCast<std::string, std::unordered_set<T> > {
+public:
+	std::unordered_set<T> operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		typename std::unordered_set<T> vec;
+		std::stringstream ss;
+		for (size_t i = 0; i < node.size(); ++i) {
+			ss.str("");
+			ss << node[i];
+			vec.insert(LexicalCast<std::string, T>()(ss.str()));
+		}
+		return vec;
+	}
+};
+
+template<class T>
+class LexicalCast<std::unordered_set<T>, std::string> {
+public:
+	std::string operator() (const std::unordered_set<T>&  v) {
+		YAML::Node node;
+		for (auto& i : v) {
+			node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+		}
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+template<class K, class V>
+class LexicalCast<std::string, std::map<K, V> > {
+public:
+	std::map<K, V> operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		typename std::map<K, V> vec;
+		std::stringstream ss;
+		for (auto it = node.begin(); it != node.end(); ++it) {
+			ss.str("");
+			ss << it->second;
+			vec.insert(std::make_pair(LexicalCast<std::string, K>()(it->first.Scalar()), LexicalCast<std::string, V>()(ss.str())));
+		}
+		return vec;
+	}
+};
+
+template<class K, class V>
+class LexicalCast<std::map<K, V>, std::string> {
+public:
+	std::string operator() (const std::map<K, V>&  v) {
+		YAML::Node node;
+		for (auto& i : v) {
+			node[i.first] = YAML::Load(LexicalCast<V, std::string>()(i.second));
+		}
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+template<class T>
+class LexicalCast<std::string, std::unordered_map<std::string, T> > {
+public:
+	std::unordered_map<std::string, T> operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		typename std::unordered_map<std::string, T> vec;
+		std::stringstream ss;
+		for (auto it = node.begin(); it != node.end(); ++it) {
+			ss.str("");
+			ss << it->second;
+			vec.insert(std::make_pair(it->first.Scalar(), LexicalCast<std::string, T>()(ss.str())));
+		}
+		return vec;
+	}
+};
+
+template<class T>
+class LexicalCast<std::unordered_map<std::string, T>, std::string> {
+public:
+	std::string operator() (const std::unordered_map<std::string, T>&  v) {
+		YAML::Node node;
+		for (auto& i : v) {
+			node[i.first] = YAML::Load(LexicalCast<T, std::string>()(i.second));
+		}
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+// FromStr T operator()(const std::string&)
+// ToStr std::string operator()(const T&)
+template<class T, class FromStr = LexicalCast<std::string, T>, class ToStr = LexicalCast<T, std::string> >
 class ConfigVar : public ConfigVarBase {
 public:
 	typedef std::shared_ptr<ConfigVar> ptr;
+	typedef std::function<void (const T& old_value, const T& new_value)> on_change_cb;
 
 	ConfigVar(const std::string& name, const T& default_value, const std::string& description = "") : ConfigVarBase(name, description), m_val(default_value) { }
-	std::string toString() {
+	std::string toString() override {
 		try {
-			return boost::lexical_cast<std::string>(m_val);
+			// return boost::lexical_cast<std::string>(m_val);
+			return ToStr()(m_val);
 		} catch (std::exception& e) {
 			ABOO_LOG_ERROR(ABOO_LOG_ROOT()) << "ConfigVar::toString exception" << e.what() << " convert: " << typeid(m_val).name() << "to string";
 		}
@@ -43,7 +245,8 @@ public:
 
 	bool fromString(const std::string& val) override {
 		try {
-			m_val = boost::lexical_cast<T>(val);
+			// m_val = boost::lexical_cast<T>(val);
+			setValue(FromStr()(val));
 			return true;
 		} catch (std::exception& e) {
 			ABOO_LOG_ERROR(ABOO_LOG_ROOT()) << "ConfigVar::toString exception" << e.what() << " convert: string to " << typeid(m_val).name();
@@ -52,9 +255,35 @@ public:
 	}
 
 	const T getValue() const { return m_val; }
-	void setValue(T& val) { m_val = val; }
+	void setValue(const T& val) {
+		if (val == m_val) return;
+		for (auto& i : m_cbs) {
+			i.second(m_val, val);
+		}
+		m_val = val;
+	}
+	std::string getTypeName() const override { return typeid(T).name(); }
+
+	void addListener(uint64_t key, on_change_cb cb) {
+		m_cbs[key] = cb;
+	}
+
+	void delListener(uint64_t key) {
+		m_cbs.erase(key);
+	}
+
+	void clearListener() {
+		m_cbs.clear();
+	}
+
+	on_change_cb getListener(uint64_t key) {
+		auto it = m_cbs.find(key);
+		return it == m_cbs.end() ? nullptr : it->second;
+	}
 private:
 	T m_val;
+	// 回调变更函数组
+	std::map<uint64_t, on_change_cb> m_cbs;
 };
 
 class Config {
@@ -63,10 +292,16 @@ public:
 
 	template<class T>
 	static typename ConfigVar<T>::ptr Lookup(const std::string& name, const T& default_value, const std::string& description = "") {
-		auto tmp = Lookup<T>(name);
-		if (tmp) {
-			ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "Lookup name=" << name << " exists";
-			return tmp;
+		auto it = s_datas.find(name);
+		if (it != s_datas.end()) {
+			auto tmp = std::dynamic_pointer_cast<ConfigVar<T> >(it->second);
+			if (tmp) {
+				ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "Lookup name=" << name << " exists";
+				return tmp;
+			} else {
+				ABOO_LOG_ERROR(ABOO_LOG_ROOT()) << "Lookup name=" << name << " exists but type not " << typeid(T).name() << " real_type=" << it->second->getTypeName() << ' ' << it->second->toString();
+				return nullptr;
+			}
 		}
 
 		if (name.find_first_not_of("abcdefghijklmnopqrstuvwxyz._0123456789") != std::string::npos) {
--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -3,7 +3,16 @@
 #include <yaml-cpp/yaml.h>
 
 aboo::ConfigVar<int>::ptr g_int_value_config = aboo::Config::Lookup("system.port", (int)8080, "system port");
+aboo::ConfigVar<float>::ptr g_int_valuex_config = aboo::Config::Lookup("system.port", (float)8080, "system port");
 aboo::ConfigVar<float>::ptr g_float_value_config = aboo::Config::Lookup("system.value", (float)10.2f, "system value");
+aboo::ConfigVar<std::vector<int> >::ptr g_int_vec_value_config = aboo::Config::Lookup("system.int_vec", std::vector<int>{1, 2}, "system int vec");
+aboo::ConfigVar<std::list<int> >::ptr g_int_list_value_config = aboo::Config::Lookup("system.int_list", std::list<int>{1, 2}, "system int vec");
+aboo::ConfigVar<std::set<int> >::ptr g_int_set_value_config = aboo::Config::Lookup("system.int_set", std::set<int>{1, 2}, "system int set");
+aboo::ConfigVar<std::unordered_set<int> >::ptr g_int_uset_value_config = aboo::Config::Lookup("system.int_uset", std::unordered_set<int>{1, 2}, "system int uset");
+aboo::ConfigVar<std::map<std::string, int> >::ptr g_str_int_map_value_config = aboo::Config::Lookup("system.str_int_map", std::map<std::string, int>{{"k", 2}}, "system str int map");
+aboo::ConfigVar<std::map<int, std::string> >::ptr g_int_str_map_value_config = aboo::Config::Lookup("system.int_str_map", std::map<int, std::string>{{2, "K"}, {3, "V"}}, "system int str map");
+aboo::ConfigVar<std::map<int, char> >::ptr g_int_char_map_value_config = aboo::Config::Lookup("system.int_char_map", std::map<int, char>{{1, 'a'}, {2, 'b'}, {3, 'c'}}, "system int char map");
+aboo::ConfigVar<std::unordered_map<std::string, int> >::ptr g_str_int_umap_value_config = aboo::Config::Lookup("system.str_int_umap", std::unordered_map<std::string, int>{{"k", 3}}, "system str int umap");
 
 void print_yaml(const YAML::Node& node, int level) {
 	if (node.IsScalar()) {
@@ -31,19 +40,135 @@ void test_yaml() {
 }
 
 void test_config() {
-	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
-	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "before: " << g_float_value_config->toString();
+//	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
+//	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "before: " << g_float_value_config->toString();
+
+#define XX(g_var, name, prefix) \
+	{ \
+		auto& v = g_var->getValue(); \
+		for (auto& i : v) { \
+			ABOO_LOG_INFO(ABOO_LOG_ROOT()) << #prefix " " #name ": " << i; \
+		} \
+		ABOO_LOG_INFO(ABOO_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString(); \
+	}
+
+#define XX_M(g_var, name, prefix) \
+	{ \
+		auto& v = g_var->getValue(); \
+		for (auto& i : v) { \
+			ABOO_LOG_INFO(ABOO_LOG_ROOT()) << #prefix " " #name ": {" << i.first << " - " << i.second << '}'; \
+		} \
+		ABOO_LOG_INFO(ABOO_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString(); \
+	}
+/*
+	XX(g_int_vec_value_config, int_vec, before);
+	XX(g_int_list_value_config, int_list, before);
+	XX(g_int_set_value_config, int_set, before);
+	XX(g_int_uset_value_config, int_uset, before);
+*/	XX_M(g_str_int_map_value_config, str_int_map, before);
+	XX_M(g_int_str_map_value_config, int_str_map, before);
+	XX_M(g_int_char_map_value_config, int_char_map, before);
+//	XX_M(g_str_int_umap_value_config, str_int_umap, before);
 
 	YAML::Node root = YAML::LoadFile("/home/hb/cpp_server_frame/bin/conf/log.yml");
 	aboo::Config::LoadFromYaml(root);
 
-	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
-	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "after: " << g_float_value_config->toString();
+//	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
+//	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "after: " << g_float_value_config->toString();
+
+/*	XX(g_int_vec_value_config, int_vec, after);
+	XX(g_int_list_value_config, int_list, after);
+	XX(g_int_set_value_config, int_set, after);
+	XX(g_int_uset_value_config, int_uset, after);
+*/	XX_M(g_str_int_map_value_config, str_int_map, after);
+	XX_M(g_int_str_map_value_config, int_str_map, after);
+	XX_M(g_int_char_map_value_config, int_char_map, after);
+//	XX_M(g_str_int_umap_value_config, str_int_umap, after);
+}
+
+class Person {
+public:
+	std::string m_name = "";
+	int m_age = 0;
+	bool m_sex = 0;
+
+	std::string toString() const {
+		std::stringstream ss;
+		ss << "[ Person name=" << m_name
+		   << " age=" << m_age
+		   << " sex=" << m_sex
+		   << "]";
+		return ss.str();
+	}
+
+	bool operator== (const Person& oth) const {
+		return m_name == oth.m_name && m_age == oth.m_age && m_sex == oth.m_sex;
+	}
+};
+
+namespace aboo {
+
+template<>
+class LexicalCast<std::string, Person > {
+public:
+	Person operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		Person p;
+		p.m_name = node["name"].as<std::string>();
+		p.m_age = node["age"].as<int>();
+		p.m_sex = node["sex"].as<bool>();
+		return p;
+	}
+};
+
+template<>
+class LexicalCast<Person, std::string> {
+public:
+	std::string operator() (const Person&  p) {
+		YAML::Node node;
+		node["name"] = p.m_name;
+		node["age"] = p.m_age;
+		node["sex"] = p.m_sex;
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+}
+aboo::ConfigVar<Person>::ptr g_person = aboo::Config::Lookup("class.person", Person(), "class person");
+aboo::ConfigVar<std::map<std::string, Person> >::ptr g_person_map = aboo::Config::Lookup("class.map", std::map<std::string, Person>(), "class person");
+
+void test_class() {
+	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "before: " << g_person->getValue().toString() << " - " << g_person->toString();
+
+#define XX_PM(g_var, prefix) \
+	{ \
+			auto &m = g_var->getValue(); \
+		for (auto& i : m) { \
+			ABOO_LOG_INFO(ABOO_LOG_ROOT()) << prefix << ": " << i.first << " - " << i.second.toString(); \
+		} \
+		ABOO_LOG_INFO(ABOO_LOG_ROOT()) << prefix << ": size=" << m.size(); \
+	}
+
+	g_person->addListener(10, [](const Person& old_value, const Person& new_value){
+				ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "old_value=" << old_value.toString() << " new_value=" << new_value.toString();
+			});
+
+	XX_PM(g_person_map, "class map before");
+
+	YAML::Node root = YAML::LoadFile("/home/hb/cpp_server_frame/bin/conf/log.yml");
+	aboo::Config::LoadFromYaml(root);
+
+	ABOO_LOG_INFO(ABOO_LOG_ROOT()) << "after: " << g_person->getValue().toString() << " - " << g_person->toString();
+	XX_PM(g_person_map, "class map after");
+
 }
 
 int main(int argc, char **argv) {
 	test_config();
 	// test_yaml();
+	// test_class();
 
 	return 0;
 }
